2531c77bed4ac690836674ca400e7cd1501df3aa
linarchingizov1-ux <linarchingizov1@gmail.com>
2026-03-26 09:07:02 +0300

    export moment

--- lib/mp_camera_scanner.dart
+++ lib/mp_camera_scanner.dart
@@ -3,3 +3,4 @@ export 'src/widgets/app_camera_preview.dart';
 export 'src/camera_found.dart';
 export 'package:camera/camera.dart';
 export 'src/widgets/scanner_view.dart';
+export 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
